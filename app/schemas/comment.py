1b3fee192413bf6a3c6a5672e7b75a40bf35c108
vishal_Sonone <vishaljsonone@gmail.com>
2025-11-19 13:54:08 +0530

    implement comment crud operations

--- app/schemas/comment.py
+++ app/schemas/comment.py
@@ -5,14 +5,17 @@ import uuid
 class CommentBase(BaseModel):
     content: str
 
+
 class CommentCreate(CommentBase):
     article_id: uuid.UUID
+
 
 class CommentRead(CommentBase):
     id: uuid.UUID
     user_id: uuid.UUID
     article_id: uuid.UUID
     created_at: datetime
+    updated_at: datetime
 
     class Config:
         orm_mode = True
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-from app.api import auth,users,category,article
+from app.api import auth,users,category,article,comment
 app=FastAPI()
 
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(category.router)
 app.include_router(article.router)
+app.include_router(comment.router)
+
 @app.get('/')
 def root():
     return {
